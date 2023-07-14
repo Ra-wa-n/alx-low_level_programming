7fbde5128b334f3ecae7f6dd9f2c715edb7d3c38
Ra-wa-n <rawannabil467@gmail.com>
2023-07-14 05:46:32 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+* Description: number
+*
+* Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	int n = o;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
