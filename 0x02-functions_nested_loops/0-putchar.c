67d6c7bdab716ac22a549789953c7018786ef604
Ra-wa-n <rawannabil467@gmail.com>
2023-07-18 05:19:54 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+*main - Entry point
+*
+* Description: putchar
+*
+* Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++;)
+		_putchar(s[ch]);
+	_putchar('\n');
+
+	return (0);
+}
